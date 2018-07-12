06936d3cdbbdabe2a1bb158ed5818723d35c7140
Serge Hänni <serge.haenni@simplificator.com>
2018-07-12 23:19:37 +0200

    Changed version to indicate an alpha.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirAleDummy.MixProject do
   def project do
     [
       app: :elixir_ale_dummy,
-      version: "0.1.2",
+      version: "0.1.2-alpha",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
